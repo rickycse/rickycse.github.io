21e5714843f6e1529e6ae7fbef7d9d96b0109a67
Ricky Tran <rickie.tran7@gmail.com>
2023-01-18 23:43:44 -0500

    website design overhaul, added nav and tldr

--- js/components/blocks.js
+++ js/components/blocks.js
@@ -8,68 +8,121 @@ Vue.component('textblock',{
 
     template: 
     `<div class="items">
-        <div class="item-title">{{text.title}}</div>
-        <div class="item-subtitle">{{text.subtitle}}</div>
-        <ul class="item-body">
-            <li v-for="point in text.points">{{point}}</li>
-        </ul>
-        <ul class="item-tags">
-            <li v-for="tag in text.tags">{{tag}}</li>
-        </ul>
-    </div>`
+        <div class="text-container">
+            <div class="item-title">{{text.title}}</div>
+            <div class="item-subtitle">{{text.subtitle}}</div>
+            <ul class="item-body">
+                <li v-for="point in text.points">{{point}}</li>
+            </ul>
+            <ul class="item-tags">
+                <li v-for="tag in text.tags" :style="{ 'backgroundColor': text.color}">{{tag}}</li>
+            </ul>
+        </div>
+        <div class="image-container">
+            <img v-bind:src="text.image">
+        </div>
+    </div>
+    `
 });
+
+Vue.component('tags',{
+    props: ['title'],
+    data: function() {
+        return {
+            tags: tags[this.title]
+        }
+    },
+
+    template: 
+    `
+    <ul class="home-tags">
+        <li v-for="tag in tags.tags" :style="{ 'backgroundColor': tags.color}">{{tag}}</li>
+    </ul>
+    `
+});
+
+const tags = {
+    TLDR: {
+        tags: ['Python', 'Java', 'JavaScript', 'HTML/CSS', 'MongoDB', 'MySQL', 'React.js', 'Vue.js', 'Git', 'Material UI'],
+        color: '#565C5B'
+    }
+}
 
 const text = {
     YORK: {
         title: 'York University',
         subtitle: 'Hons. B.Sc Computer Science Student',
-        points: ['CGPA: 3.16 / 4.0', 'Notable Courses: Discrete Mathematics, Programming for Mobile Computing, Applied Calculus I & II, Intro. to Computer Science & Programming.']
+        points: ['CGPA: 3.16 / 4.0', 'Notable Courses: Advanced Object-Oriented Programming, Fundamentals of Data Structures, Programming for Mobile Computing, Discrete Mathematics, Introduction to Computer Science and Programming.'],
+        image: '../media/lassondeschool.jpg',
     },
 
     BIKEHUB: {
         title: 'CultureLink Bike Hub',
         subtitle: 'Bike Mechanic and Front-Desk Attendant',
-        points: ['Demonstrated the ability to communicate and assist visitors with mechanical concerns.', 'Successfully salvaged, repaired, and assembled over 50 different bicycles.','Supported the biannual bicycle workshops for newcomers by taking an active role in demonstrating assembly, repairs, and salvages.']
+        points: ['Demonstrated the ability to communicate and assist visitors with mechanical concerns.', 'Successfully salvaged, repaired, and assembled over 50 different bicycles.','Supported the biannual bicycle workshops for newcomers by taking an active role in demonstrating assembly, repairs, and salvages.'],
+        image: '../media/bikehubico.png',
+    },
+
+    YORKUIT: {
+        title: 'York UIT Client Services',
+        subtitle: 'Computing Support Assistant',
+        points: ['Spearheaded troubleshooting to cut downtime on computer and network-related issues',
+        'Demonstrated the ability to communicate technical information in plain language.',
+        'Maintained up-to-date knowledge of computer applications, company services, and service procedures to provide clients with the utmost support and customer satisfaction'],
+        image: '../media/yorkuit.png',
     },
 
     PYGAME: {
         title: 'Jellyfish Blaster',
         subtitle: 'Animated Python Game',
         points: ['Successfully created an environment to load and transform two-dimensional game assets, with or without user interaction.','Facilitated the process of fetching and loading animations by organizing bulk image folders into specialized folders per animation with the OS module.'],
-        tags: ['Python', 'Pygame', 'Sprites', 'Os Module']
+        tags: ['Python', 'Pygame', 'Sprites', 'Os Module'],
+        image: '../media/arcadepygame.jpg',
+        color: '#1C658C',
     }, 
 
     FINTECH: {
         title: 'Fintechify',
         subtitle: 'Banking Application',
         points: ['Demonstrated the ability to fetch currency conversion data from an online finance API', 'Designed a user-friendly, interactive banking application featuring deposits, withdrawals, money conversion, etc.'],
-        tags: ['Java', 'OkHttp3', 'Android Widgets', 'Figma']
+        tags: ['Java', 'OkHttp3', 'Android Widgets', 'Figma'],
+        image: '../media/fintechify.png',
+        color: '#645CAA',
     },
 
     DISCORDBOT: {
         title: 'Tabikat Bot',
         subtitle: 'Personal Discord.js Bot',
         points: ['Utilized Discord API and MongoDB documentation to create an interactive message bot.','Implemented user-friendly and convenient commands (grade calculator, reminders, etc).'],
-        tags: ['Discord.js', 'JavaScript', 'MongoDB', 'Git', 'Ms Module']
+        tags: ['Discord.js', 'JavaScript', 'MongoDB', 'Git', 'Ms Module'],
+        image: '../media/discordbot.jpg',
+        color: '#357C3C',
     },
 
     GITPAGES: {
         title: 'Ricky Tran',
         subtitle: 'Personal Portfolio',
         points: ['Developed a simple website resume using HTML, CSS, and JavaScript.', 'Utilized Github Pages to statically host a website.','Implemented Vue Components to simplify the process of adding onto the website'],
-        tags: ['HTML', 'JavaScript', 'CSS', 'Vue Components', 'Git Pages'], 
+        tags: ['HTML', 'JavaScript', 'CSS', 'Vue.js', 'Git Pages'],
+        image: '../media/webfolio.jpg',
+        color: '#B33030',
     },
 
     ARDUINO: {
         title: 'Arduino Uno',
         subtitle: 'Sound Activated LEDs',
         points: ['Built a sound-activated LED strip using the Arduino IDE and language.', 'Utilized MOSFETs to control LEDs and prevent overvoltage to the Arduino Board.'],
-        tags: ['C++','Arduino Uno', 'IRLZ44N', 'KY-037 Sound Sensor'], 
+        tags: ['C++','Arduino Uno', 'IRLZ44N', 'KY-037 Sound Sensor'],
+        image: '../media/arduino.png',
+        color: '#1C658C',
     }
 }
 
+const TAGS = new Vue({el: '#TLDR-TAGS'});
+
 const LASSONDE = new Vue({el: '#LASSONDE'});
 const BIKEHUB = new Vue({el: '#BIKEHUB'});
+const YORKUIT = new Vue({el: '#YORKUIT'});
 const PYGAME = new Vue({el: '#PYGAME'});
 const FINTECH = new Vue({el: '#FINTECH'});
 const DISCORD = new Vue({el: '#DISCORD'});
